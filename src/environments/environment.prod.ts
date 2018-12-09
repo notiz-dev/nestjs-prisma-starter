0114f19716a607a23153056a7e75f4a0e199c7e5
Marc Stammerjohann <marcstammerjohann@web.de>
2018-12-09 02:01:43 +0100

    fix(env): add saltOrRounds back to env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
     production: true,
     secret: 'appsecret321',
     expiresIn: 3600,
+    saltOrRounds: 10,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
     production: false,
     secret: 'appsecret321',
     expiresIn: 3600,
+    saltOrRounds: 10,
 };
