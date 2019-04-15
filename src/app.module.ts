0eafc7014c9e642299d476252e3c1b78ad71686b
Marc Stammerjohann <marcstammerjohann@web.de>
2019-04-15 21:43:15 +0200

    chore(graphql): add graphql lib

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
+import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      debug: true,
+      playground: true,
+      context: ({ req }) => ({ req }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
