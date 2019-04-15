655090c2836da76d73d2c269694219d89ad6b63b
Marc Stammerjohann <marcstammerjohann@web.de>
2019-04-15 21:46:21 +0200

    chore(graphq): generate typings

---
+++ generate-typings.ts
@@ -0,0 +1,10 @@
+
+import { GraphQLDefinitionsFactory } from '@nestjs/graphql';
+import { join } from 'path';
+
+const definitionsFactory = new GraphQLDefinitionsFactory();
+definitionsFactory.generate({
+    typePaths: ['./src/**/*.graphql'],
+    path: join(process.cwd(), 'src/generated/graphql.ts'),
+    outputAs: 'class',
+});
---
+++ src/generated/graphql.ts
@@ -0,0 +1,12 @@
+
+/** ------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+export abstract class IQuery {
+    abstract helloWorld(): string | Promise<string>;
+
+    abstract temp__(): boolean | Promise<boolean>;
+}
