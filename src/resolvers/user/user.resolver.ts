279921899d7bfdc6039893ee34d249d1f313cb7b
Marc Stammerjohann <marcstammerjohann@web.de>
2020-02-09 13:02:25 +0100

    feat(user): change password

--- src/resolvers/user/user.resolver.ts
+++ src/resolvers/user/user.resolver.ts
@@ -1,18 +1,43 @@
 import { PrismaService } from './../../services/prisma.service';
 import { GqlAuthGuard } from '../../guards/gql-auth.guard';
-import { Resolver, Query, ResolveProperty, Parent } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  ResolveProperty,
+  Parent,
+  Mutation,
+  Args
+} from '@nestjs/graphql';
 import { UseGuards } from '@nestjs/common';
 import { UserEntity } from '../../decorators/user.decorator';
 import { User } from './../../models/user';
+import { ChangePasswordInput } from './dto/change-password.input';
+import { UserService } from 'src/services/user.service';
 
 @Resolver(of => User)
 @UseGuards(GqlAuthGuard)
 export class UserResolver {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private userService: UserService,
+    private prisma: PrismaService
+  ) {}
 
   @Query(returns => User)
   async me(@UserEntity() user: User): Promise<User> {
     return user;
+  }
+
+  @UseGuards(GqlAuthGuard)
+  @Mutation(returns => User)
+  async changePassword(
+    @UserEntity() user: User,
+    @Args('data') changePassword: ChangePasswordInput
+  ) {
+    return this.userService.changePassword(
+      user.id,
+      user.password,
+      changePassword
+    );
   }
 
   @ResolveProperty('posts')
