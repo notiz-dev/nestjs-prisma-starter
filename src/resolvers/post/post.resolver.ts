a523d043568fa9780985ce475c81057cfc6e16a5
Marc Stammerjohann <marcstammerjohann@web.de>
2020-03-27 11:01:08 +0100

    fix: comment out pagination connection

--- src/resolvers/post/post.resolver.ts
+++ src/resolvers/post/post.resolver.ts
@@ -5,39 +5,39 @@ import { UserIdArgs } from '../../models/args/userid-args';
 import { Resolver, Query, Parent, Args, ResolveField } from '@nestjs/graphql';
 import { Post } from './../../models/post';
 import { findManyCursor } from '../../common/pagination/find-many-cursor';
-import { PostConnection } from '../../models/pagination/post.pagination';
 import { PostOrder } from '../../models/inputs/post.order';
 
 @Resolver((of) => Post)
 export class PostResolver {
   constructor(private prisma: PrismaService) {}
 
-  @Query(returns => PostConnection)
-  async publishedPosts(
-    @Args() { skip, after, before, first, last }: PaginationArgs,
-    @Args({ name: 'query', type: () => String, nullable: true })
-    query: string,
-    @Args({
-      name: 'orderBy',
-      type: () => PostOrder,
-      nullable: true
-    })
-    orderBy: PostOrder
-  ) {
-    return await findManyCursor(
-      args =>
-        this.prisma.post.findMany({
-          include: { author: true },
-          where: {
-            published: true,
-            title: { contains: query || '' }
-          },
-          orderBy: orderBy ? { [orderBy.field]: orderBy.direction } : null,
-          ...args
-        }),
-      { first, last, before, after }
-    );
-  }
+  // TODO PostConnection not working
+  // @Query(returns => PostConnection)
+  // async publishedPosts(
+  //   @Args() { skip, after, before, first, last }: PaginationArgs,
+  //   @Args({ name: 'query', type: () => String, nullable: true })
+  //   query: string,
+  //   @Args({
+  //     name: 'orderBy',
+  //     type: () => PostOrder,
+  //     nullable: true
+  //   })
+  //   orderBy: PostOrder
+  // ) {
+  //   return await findManyCursor(
+  //     args =>
+  //       this.prisma.post.findMany({
+  //         include: { author: true },
+  //         where: {
+  //           published: true,
+  //           title: { contains: query || '' }
+  //         },
+  //         orderBy: orderBy ? { [orderBy.field]: orderBy.direction } : null,
+  //         ...args
+  //       }),
+  //     { first, last, before, after }
+  //   );
+  // }
 
   @Query((returns) => [Post])
   userPosts(@Args() id: UserIdArgs) {
