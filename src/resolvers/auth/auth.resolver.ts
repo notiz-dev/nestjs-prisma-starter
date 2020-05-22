5377ceb5c292dba61050dff196d1af7be1dbfede
Leo Hui <leohxj@gmail.com>
2020-05-22 11:00:59 +0800

    feat: add validate and generate refresh token

--- src/resolvers/auth/auth.resolver.ts
+++ src/resolvers/auth/auth.resolver.ts
@@ -1,4 +1,5 @@
 import { Auth } from '../../models/auth.model';
+import { Token } from '../../models/token.model';
 import { LoginInput } from './dto/login.input';
 import {
   Resolver,
@@ -17,22 +18,33 @@ export class AuthResolver {
   @Mutation((returns) => Auth)
   async signup(@Args('data') data: SignupInput) {
     data.email = data.email.toLowerCase();
-    const token = await this.auth.createUser(data);
+    const { accessToken, refreshToken } = await this.auth.createUser(data);
     return {
-      token,
+      accessToken,
+      refreshToken,
     };
   }
 
   @Mutation((returns) => Auth)
   async login(@Args('data') { email, password }: LoginInput) {
-    const token = await this.auth.login(email.toLowerCase(), password);
+    const { accessToken, refreshToken } = await this.auth.login(
+      email.toLowerCase(),
+      password
+    );
+
     return {
-      token,
+      accessToken,
+      refreshToken,
     };
+  }
+
+  @Mutation((returns) => Token)
+  async refreshToken(@Args('token') token: string) {
+    return this.auth.refreshToken(token);
   }
 
   @ResolveField('user')
   async user(@Parent() auth: Auth) {
-    return await this.auth.getUserFromToken(auth.token);
+    return await this.auth.getUserFromToken(auth.accessToken);
   }
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,23 +2,27 @@ import {
   Injectable,
   NotFoundException,
   BadRequestException,
-  ConflictException
+  ConflictException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PasswordService } from './password.service';
 import { SignupInput } from '../resolvers/auth/dto/signup.input';
 import { PrismaService } from './prisma.service';
 import { User } from '@prisma/client';
+import { Token } from '../models/token.model';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly jwtService: JwtService,
     private readonly prisma: PrismaService,
-    private readonly passwordService: PasswordService
+    private readonly passwordService: PasswordService,
+    private readonly configService: ConfigService
   ) {}
 
-  async createUser(payload: SignupInput): Promise<string> {
+  async createUser(payload: SignupInput): Promise<Token> {
     const hashedPassword = await this.passwordService.hashPassword(
       payload.password
     );
@@ -28,17 +32,19 @@ export class AuthService {
         data: {
           ...payload,
           password: hashedPassword,
-          role: 'USER'
-        }
+          role: 'USER',
+        },
       });
 
-      return this.jwtService.sign({ userId: user.id });
+      return this.generateToken({
+        userId: user.id,
+      });
     } catch (error) {
       throw new ConflictException(`Email ${payload.email} already used.`);
     }
   }
 
-  async login(email: string, password: string): Promise<string> {
+  async login(email: string, password: string): Promise<Token> {
     const user = await this.prisma.user.findOne({ where: { email } });
 
     if (!user) {
@@ -54,7 +60,9 @@ export class AuthService {
       throw new BadRequestException('Invalid password');
     }
 
-    return this.jwtService.sign({ userId: user.id });
+    return this.generateToken({
+      userId: user.id,
+    });
   }
 
   validateUser(userId: string): Promise<User> {
@@ -64,5 +72,30 @@ export class AuthService {
   getUserFromToken(token: string): Promise<User> {
     const id = this.jwtService.decode(token)['userId'];
     return this.prisma.user.findOne({ where: { id } });
+  }
+
+  generateToken(payload: object): Token {
+    const accessToken = this.jwtService.sign(payload);
+
+    const refreshToken = this.jwtService.sign(payload, {
+      expiresIn: this.configService.get('JWT_REFRESH_IN'),
+    });
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  refreshToken(token: string) {
+    try {
+      const { userId } = this.jwtService.verify(token);
+
+      return this.generateToken({
+        userId,
+      });
+    } catch (e) {
+      throw new UnauthorizedException();
+    }
   }
 }
