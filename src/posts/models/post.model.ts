2f18b3e0485521708f6d95d1278126d858c79244
luluhoc <lucjan.grzesik@gmail.com>
2023-01-24 11:10:11 +0000

    refactor(): class-validator and post model refactor

--- src/posts/models/post.model.ts
+++ src/posts/models/post.model.ts
@@ -4,15 +4,8 @@ import { BaseModel } from 'src/common/models/base.model';
 
 @ObjectType()
 export class Post extends BaseModel {
-  @Field()
   title: string;
-
-  @Field(() => String, { nullable: true })
   content?: string | null;
-
-  @Field(() => Boolean)
   published: boolean;
-
-  @Field(() => User, { nullable: true })
   author?: User | null;
 }
