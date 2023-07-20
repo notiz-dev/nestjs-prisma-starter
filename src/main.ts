b6c700c98ba23599e091e9adf2fdef90fd2875fe
Alex Ruheni <33921841+ruheni@users.noreply.github.com>
2023-07-20 10:00:43 +0200

    chore: update PrismaService shutdown hooks (#484)
    * chore: update prisma-related dependencies
    
    * chore: remove PrismaService shutdown hooks

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { PrismaClientExceptionFilter, PrismaService } from 'nestjs-prisma';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 import { AppModule } from './app.module';
 import type {
   CorsConfig,
@@ -17,8 +17,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   // enable shutdown hook
-  const prismaService: PrismaService = app.get(PrismaService);
-  await prismaService.enableShutdownHooks(app);
+  app.enableShutdownHooks();
 
   // Prisma Client Exception Filter for unhandled exceptions
   const { httpAdapter } = app.get(HttpAdapterHost);
