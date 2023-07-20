74235d13bb46686f270ef88704fd6a05b3b994b0
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2023-07-20 10:24:17 +0200

    update to nest v10, closes #482 #483

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,13 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'prettier'
   ],
   root: true,
   env: {
